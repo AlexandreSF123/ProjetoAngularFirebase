a64997eb156748a6065c9999e8e9294fe1930f75
AlexandreSF123 <alexandresantosfonsec47@gmail.com>
2026-02-26 18:22:36 -0300

    layout basico

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -12,17 +12,11 @@ import { Router } from '@angular/router';
 })
 export class HomePage {
 
-  pokemon:any = {
-    nome: null,
-    poder: null
-  };
-
-  constructor( 
-    public crudService: CrudService
-  ){ }
-
-  enviar() {
-    this.crudService.insert(this.pokemon, 'pokemons');
-  }
+  projeto: object[] = [
+    { descricao: "tomar banho", local: "casa", hora_inicio: "19:10", hora_termino: "20:20", status : "em espera"},
+    { descricao: "janatr", local: "casa", hora_inicio: "19:00", hora_termino: "19:10", status : "em espera"},
+    { descricao: "estudar", local: "FATEC", hora_inicio: "13:10", hora_termino: "16:20", status : "em espera"},
+    { descricao: "almocar", local: "casa", hora_inicio: "12:00", hora_termino: "12:30", status : "concluido"}
+  ];
 
 }
